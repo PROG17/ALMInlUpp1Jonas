751a896a4dbba1d6ab73c9f13b00a6b72500b758
Jonas Lindström <Jonas.Lindstrom@yh.nackademin.se>
2018-11-07 13:16:09 +0100

    Added Test Project

---
+++ InlUpp1TestJonas/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InlUpp1TestJonas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
