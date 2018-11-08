40c80456611d73b13a112242a7a5eec9a71e737a
Jonas Lindström <Jonas.Lindstrom@yh.nackademin.se>
2018-11-08 17:20:01 +0100

    Feature 1 ready

---
+++ ALMInlUpp1Jonas/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALMInlUpp1Jonas.Models
+{
+    public class Account
+    {
+        public int AccountNr { get; internal set; }
+        public decimal Balance { get; internal set; }
+        public Customer Owner { get; internal set; }
+    }
+}
